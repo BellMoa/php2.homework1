d1c4d8e39f4d9ee8e5924cb5659040057ca0777d
Артем <insidia93@gmail.com>
2017-01-25 14:08:06 +0200

    Добавлена визуализация данных с помощью таблицы

--- view/index.php
+++ view/index.php
@@ -5,8 +5,19 @@
     <title>Title</title>
 </head>
 <body>
-<?php foreach ($news as $news1): ?>
-    <?php echo $news1['title'].'<br>'.$news1['text']?>
-<?php endforeach;?>
+<table style="border: 1px solid black;border-collapse: collapse;width: 400px">
+    <?php foreach ($news as $news1): ?>
+    <tr>
+        <td style="border: 1px solid black"><?php echo $news1['title'] ?></td>
+        <td style="border: 1px solid black"><?php echo $news1['date'].' '.$news1['time']?></td>
+    </tr>
+    <tr>
+        <td colspan="2" style="border: 1px solid black"><?php echo $news1['text']?></td>
+    </tr>
+    <?php endforeach;?>
+</table>
+
+
+
 </body>
 </html>
