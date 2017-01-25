e1e7f3616ef8f990128a46b9a3d80d185eac6cb1
Артем <insidia93@gmail.com>
2017-01-25 15:03:27 +0200

    Добавлен контроллер проверки формы добавления новости

--- add.php
+++ add.php
@@ -1,3 +1,25 @@
 <?php
+if (!empty($_POST)){
+    $data=[];
+   if (!empty($_POST['title'])){
+    $data['title'] = $_POST['title'];
+   }
+   if (!empty($_POST['text'])){
+      $data['text'] = $_POST['text'];
+   }
+   if (!empty($_POST['date'])){
+    $data['date']=$_POST['date'];
+   }
+   if (!empty($_POST['time'])){
+       $data['time'] = $_POST['time'];
+   }
+
+   if (isset($data['title']) && isset($data['text']) && isset($data['date']) && isset($data['time'])){
+
+   }
+
+return false;
+}
+
 
 require __DIR__ . '/view/add.php';
