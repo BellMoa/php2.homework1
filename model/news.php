ee5a94c03ee6e197df0e727cb4470cc7994dceec
Артем <insidia93@gmail.com>
2017-01-25 15:28:10 +0200

    Добавлена функция которая принимает массив значений из контроллера, затем добавляет эти записи в БД.

--- model/news.php
+++ model/news.php
@@ -1,6 +1,14 @@
 <?php
 require_once  __DIR__ .'/../lib/sql.php';
 function News_getAll(){
-    $sql="SELECT * FROM news";
+    $sql="SELECT * FROM news ORDER BY date DESC";
     return getFromDB($sql);
+}
+
+function newsInsert($data){
+    $sql="INSERT INTO news (title,text,date)
+          VALUES ('".$data['title']."','".$data['text']."','".$data['date']."')
+";
+    insertIntoDB($sql);
+
 }
